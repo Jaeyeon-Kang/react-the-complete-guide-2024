a376605171cf0ce64e6f44ae760b575fbe8a2feb
Jaeyeon-Kang <wodus770@gmail.com>
2024-09-09 23:04:13 +0900

    feat: state와 refs의 차이

--- REACT-Working-with-Refs-Portals/src/components/Player.jsx
+++ REACT-Working-with-Refs-Portals/src/components/Player.jsx
@@ -13,8 +13,11 @@ export default function Player() {
 
   function handleClick() {
     setEnteredPlayerName(playerName.current.value);
+    console.log(playerName);
     playerName.current.value = "";
   }
+
+  // state는 변화가 있을 때마다 재실행이 되지만, refs는 component의 재실행을 보장하지 않는다.
 
   return (
     <section id="player">
