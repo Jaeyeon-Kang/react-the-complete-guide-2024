0d991d9cfd415cc18f94d2c28b5229cf54a8263f
Jaeyeon-Kang <wodus770@gmail.com>
2024-09-11 21:13:45 +0900

    feat: forwarding refs to custom components

--- REACT-Working-with-Refs-Portals/src/components/TimerChallenge.jsx
+++ REACT-Working-with-Refs-Portals/src/components/TimerChallenge.jsx
@@ -2,12 +2,14 @@ import { useState, useRef } from "react";
 import ResultModal from "./ResultModal";
 export default function TimerChallenge({ title, targetTime }) {
   const timer = useRef();
+  const dialog = useRef();
   const [timerStarted, setTimerStarted] = useState(false);
   const [timerExpired, setTimerExpired] = useState(false);
 
   function handleStart() {
     timer.current = setTimeout(() => {
       setTimerExpired(true);
+      dialog.current.showModal();
     }, targetTime * 1000);
 
     setTimerStarted(true);
@@ -19,10 +21,10 @@ export default function TimerChallenge({ title, targetTime }) {
 
   return (
     <>
-      {timerExpired && <ResultModal targetTIme={targetTime} result="lost" />}
+      <ResultModal ref={dialog} targetTIme={targetTime} result="lost" />
+
       <section className="challenge">
         <h2>{title}</h2>
-        {timerExpired && <p>You lost!</p>}
         <p className="challenge-time">
           {targetTime} second{targetTime > 1 ? "s" : ""}
         </p>
