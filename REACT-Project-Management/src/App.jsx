1e454cf2f4b7ae50bc4bdc7a9100e6d1f65ff48d
Jaeyeon-Kang <wodus770@gmail.com>
2024-09-19 23:34:49 +0900

    feat: styling

--- REACT-Project-Management/src/App.jsx
+++ REACT-Project-Management/src/App.jsx
@@ -1,8 +1,14 @@
+import ProjectsSidebar from "./components/ProjectSidebar";
+
 function App() {
   return (
-    <>
-      <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1>
-    </>
+    <main className="h-screen my-8 " {
+      constructor(parameters) {
+        
+      }
+    }>
+      <ProjectsSidebar />
+    </main>
   );
 }
 
